160517bf784d25e2e89dfe4eddbc557273e11d3a
choche40 <lia.joseluis@hotmail.com>
2020-02-22 15:16:24 -0600

    Dejango

---
+++ ProgramacionII/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\liajo\OneDrive\Escritorio\Informatica\ProgramacionDjango\ProgramacionDjango\ProgramacionII\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
